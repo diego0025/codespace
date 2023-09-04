1d34b4e21d8a5b691b56a0858b790239e58006cc
Diego Padilla <82690747+diego0025@users.noreply.github.com>
2023-09-04 17:20:28 +0000

    Mon, Sep 4, 2023, 5:20 PM +00:00

--- code.py
+++ code.py
@@ -1,19 +1,59 @@
-# Importing Libraries
-import instaloader
-import pandas as pd
+from instapy import InstaPy
+from instapy.util import smart_run
+from instapy.util import get_relationship_counts
+import argparse
 
-# Creating an instance of the Instaloader class
-bot = instaloader.Instaloader()
-bot.login(user="ronaldo678923", passwd="asdfghzxcvbn")
 
-# Loading a profile from an Instagram handle
-profile = instaloader.Profile.from_username(bot.context, 'diegopadilla.i')
+# utility functions
+def getPercentageCount(followers, percent):
+    return int((followers * percent) / 100)
 
-# Retrieving the usernames of all followers
-followers = [follower.username for follower in profile.get_followers()]
 
-# Converting the data to a DataFrame
-followers_df = pd.DataFrame(followers)
+# login credentials
+insta_username = 'ronaldo678923'
+insta_password = 'asdfghzxcvbn'
 
-# Storing the results in a CSV file
-followers_df.to_csv('followers.csv', index=False)
+# parse arguments and get account name and percentage of followers to be extracted
+ap = argparse.ArgumentParser()
+ap.add_argument("-a", "--account", required=True,
+                help="name of the account to be scraped for followers")
+ap.add_argument("-p", "--percentage", required=True,
+                help="percentage of followers to be scraped")
+
+args = vars(ap.parse_args())
+
+targetAccount = ""
+percentage = 100
+
+if args["account"]:
+    targetAccount = format(args["account"])
+if args["percentage"]:
+    percentage = format(args["percentage"])
+
+print('[INFO] Extracting ' + percentage + '% of the followers from ' + targetAccount)
+
+# get an InstaPy session!
+# set headless_browser=True to run InstaPy in the background
+session = InstaPy(username=insta_username,
+                  password=insta_password,
+                  headless_browser=True)
+
+print('[INFO]: Logging in..')
+with smart_run(session):
+    # get follower, following count
+    followers_count, following_count = get_relationship_counts(session.browser, targetAccount, session.logger)
+    amount = getPercentageCount(int(followers_count), int(percentage))
+    print('[INFO] Going to extract ' + str(amount) + ' followers out of ' + str(followers_count))
+
+    followers_list = session.grab_followers(username=targetAccount, amount=amount, live_match=True, store_locally=False)
+
+    print('[INFO]: Followers grabbed successfully. Saving to file..')
+
+    result = ''
+    for el in followers_list:
+        result = result + el + '\n'
+
+    with open("./" + str(targetAccount) + "_followers.txt", "w") as text_file:
+        text_file.write(result)
+
+    print('[INFO]: Followers list successfully saved to ' + targetAccount + '_followers.txt')
